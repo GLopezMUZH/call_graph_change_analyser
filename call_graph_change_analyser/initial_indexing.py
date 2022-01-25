9371ed23b6e973efe106695d57a927564ce5bd26
Gabriela López M <lopm@zhaw.ch>
2022-01-25 10:45:45 +0100

    moved download_initial_cache_source to git_util

--- call_graph_change_analyser/initial_indexing.py
+++ call_graph_change_analyser/initial_indexing.py
@@ -2,32 +2,9 @@ import os
 import pandas as pd
 from itertools import islice
 import sqlite3
-import git
-import logging
 #from stopwatch import Stopwatch
 
 from models import ProjectPaths
-
-
-def download_initial_cache_source(repo_url, path_to_cache_src_dir, only_in_branch):
-    if not os.path.exists(os.path.dirname(path_to_cache_src_dir)):
-        os.makedirs(os.path.dirname(path_to_cache_src_dir))
-        logging.info("Start git clone")
-    else:
-        if os.path.exists(os.path.join(path_to_cache_src_dir,'.git')):
-            logging.info("Reset cached source to current state")
-            g = git.Git(path_to_cache_src_dir)
-            g.checkout(only_in_branch)
-        else:
-            logging.error("path_to_cache_src_dir exist but not .git folder")
-            #stopwatch = Stopwatch()
-            #stopwatch.start()
-            git.Repo.clone_from(repo_url, path_to_cache_src_dir)
-            #stopwatch.stop()
-            #print(stopwatch.elapsed) 
-            #print(stopwatch.report())
-            logging.info("End git clone")
-
 
 
 def execute_intitial_indexing(proj_paths: ProjectPaths):

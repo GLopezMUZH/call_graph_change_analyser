dc746aabd98086d1d31a5fc7c9bbcf2c241b1432
Gabriela López M <lopm@zhaw.ch>
2022-01-31 15:10:14 +0100

    updated local ide testing

--- call_graph_change_analyser/test_app.py
+++ call_graph_change_analyser/test_app.py
@@ -9,16 +9,19 @@ from repository_mining import analyse_source_repository_data
 from utils_sql import create_db_tables
 from project_configs import *
 
-from initial_indexing import execute_intitial_indexing, download_initial_cache_source
+from initial_indexing import execute_intitial_indexing
+from git_util import download_initial_cache_source
+from repository_mining import analyse_source_repository_data
+from call_graph_parsing_util import calculate_cg_diffs
+from cg_to_commit_util import update_commit_changes_to_cg_nodes
+from utils_sql import create_db_tables
+from cg_change_coupling_util import save_cg_change_coupling
 
 # %%
 def main():
-    path_to_db_file = os.path.normpath(
-        '..\project_results\glucosio-android\.cache\callgraphdb\glucosio-android_raw_cg.db')
-    path_to_config_file = os.path.normpath('..\project_config\glucosio_small.pconfig')
+    path_to_config_file = os.path.normpath('..\project_config\glucosio_large_commits.pconfig') #glucosio_small
 
     os.path.exists(path_to_config_file)
-    os.path.exists(path_to_db_file)
 
     proj_config, proj_paths = execute_project_conf_from_file(
         str(path_to_config_file))
@@ -26,15 +29,18 @@ def main():
     # can only log after seting log file path
     logging.info('Started App ---------- {0}'.format(datetime.now()))
 
-    if True: #'-init_db_yes' in args:
-        init_db(proj_paths)
+    init_db(proj_paths)
 
-    # if '-init_index_yes' in args:
-    download_initial_cache_source(proj_config.get_repo_url(
-    ), proj_paths.get_path_to_cache_src_dir(), proj_config.get_only_in_branch())
+    download_initial_cache_source(proj_config.get_repo_url(), proj_paths.get_path_to_cache_src_dir(), proj_config.get_only_in_branch())
     execute_intitial_indexing(proj_paths)
 
     analyse_source_repository_data(proj_config=proj_config, proj_paths=proj_paths)
+
+    calculate_cg_diffs(proj_config=proj_config, proj_paths=proj_paths)
+
+    update_commit_changes_to_cg_nodes(proj_config=proj_config, proj_paths=proj_paths)
+
+    save_cg_change_coupling(proj_config=proj_config, proj_paths=proj_paths)
 
     logging.info('Finished App ---------- {0}'.format(datetime.now()))
     print('Finished App -------------{0}'.format(datetime.now()))

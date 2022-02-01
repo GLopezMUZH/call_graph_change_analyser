aa1ca7f4de9cb4077be1d947a1d10de0dd63d0f7
Gabriela López M <lopm@zhaw.ch>
2022-02-01 23:34:10 +0100

    download_initial_cache_source create dir git

--- call_graph_change_analyser/git_util.py
+++ call_graph_change_analyser/git_util.py
@@ -7,20 +7,20 @@ def download_initial_cache_source(repo_url, path_to_cache_src_dir, only_in_branc
     if not os.path.exists(os.path.dirname(path_to_cache_src_dir)):
         os.makedirs(os.path.dirname(path_to_cache_src_dir))
         logging.info("Start git clone")
+
+    if os.path.exists(os.path.join(path_to_cache_src_dir,'.git')):
+        logging.info("Reset cached source to current state")
+        g = git.Git(path_to_cache_src_dir)
+        g.checkout(only_in_branch)
     else:
-        if os.path.exists(os.path.join(path_to_cache_src_dir,'.git')):
-            logging.info("Reset cached source to current state")
-            g = git.Git(path_to_cache_src_dir)
-            g.checkout(only_in_branch)
-        else:
-            logging.error("path_to_cache_src_dir exist but not .git folder")
-            #stopwatch = Stopwatch()
-            #stopwatch.start()
-            git.Repo.clone_from(repo_url, path_to_cache_src_dir)
-            #stopwatch.stop()
-            #print(stopwatch.elapsed) 
-            #print(stopwatch.report())
-            logging.info("End git clone")
+        logging.error("path_to_cache_src_dir exist but not .git folder")
+        #stopwatch = Stopwatch()
+        #stopwatch.start()
+        git.Repo.clone_from(repo_url, path_to_cache_src_dir)
+        #stopwatch.stop()
+        #print(stopwatch.elapsed) 
+        #print(stopwatch.report())
+        logging.info("End git clone")
 
 def reset_git_to_hash(repo_url: str, path_to_cache_src_dir: str, commit_hash):
     if os.path.exists(os.path.join(path_to_cache_src_dir,'.git')):

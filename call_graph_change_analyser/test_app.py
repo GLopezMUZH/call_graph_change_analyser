7c1da33ef6b96baae0f6a3ad4716ff58df9ee8b4
Gabriela López M <lopm@zhaw.ch>
2022-02-01 23:33:36 +0100

    added remove_unparsed_git_commits

--- call_graph_change_analyser/test_app.py
+++ call_graph_change_analyser/test_app.py
@@ -14,12 +14,12 @@ from git_util import download_initial_cache_source
 from repository_mining import analyse_source_repository_data
 from call_graph_parsing_util import calculate_cg_diffs
 from cg_to_commit_util import update_commit_changes_to_cg_nodes
-from utils_sql import create_db_tables
+from utils_sql import create_db_tables, remove_unparsed_git_commits
 from cg_change_coupling_util import save_cg_change_coupling
 
 # %%
 def main():
-    path_to_config_file = os.path.normpath('..\project_config\glucosio_large_commits.pconfig') #glucosio_small
+    path_to_config_file = os.path.normpath('..\project_config\GRIP.pconfig') #glucosio_small
 
     os.path.exists(path_to_config_file)
 
@@ -35,6 +35,8 @@ def main():
     execute_intitial_indexing(proj_paths)
 
     analyse_source_repository_data(proj_config=proj_config, proj_paths=proj_paths)
+
+    remove_unparsed_git_commits(proj_config=proj_config, proj_paths=proj_paths)
 
     calculate_cg_diffs(proj_config=proj_config, proj_paths=proj_paths)
 
